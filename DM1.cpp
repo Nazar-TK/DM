8589dfdd080937f1f23f6117cdf1e5d46c94e524
Nazar-TK <55621112+Nazar-TK@users.noreply.github.com>
2019-10-09 21:32:11 +0300

    Add files via upload

---
+++ DM1.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int x = 0, y = 0, z = 0;
+	printf("Enter x: ");
+	scanf_s("%d", &x);
+	printf("Enter y: ");
+	scanf_s("%d", &y);
+	printf("Enter z: ");
+	scanf_s("%d", &z);
+	printf("x=%d, y=%d, z=%d\n", x, y, z);
+	if (   x == 0 && y == 0 && z == 0 || x == 0 && y == 0 && z == 1 || x == 0 && y == 1 && z == 0
+		|| x == 0 && y == 1 && z == 1 || x == 1 && y == 0 && z == 0 || x == 1 && y == 0 && z == 1
+		|| x == 1 && y == 1 && z == 0 || x == 1 && y == 1 && z == 1)
+		printf("False");
+	else
+		printf("Enter correct numbers");
+}
